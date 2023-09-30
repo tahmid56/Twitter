2ea6e26b5036e0b27d3262accb02910b84ae597f
Tahmid Akter <tahmidakter56@gmail.com>
2023-09-30 07:34:33 +0600

    Tweet feed design done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet ,View, FlatList} from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import tweets from '../../assets/data/tweets';
+import Tweet from '../../components/Tweet';
 
+const tweet = tweets[0]
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={styles.page}>
+        {/* <Tweet tweet={tweets[0]}/> */}
+        <FlatList data={tweets} renderItem={({item}) => <Tweet tweet={item}/>}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    page:{
+        backgroundColor: "white",
+        flex: 1
+    }
 });
