ffe13a599d8a998603dfc0208b3d25ce581850e9
Tahmid Akter <tahmidakter56@gmail.com>
2023-10-01 06:37:36 +0600

    Icon button component added

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,7 @@
 import {View, Text, Image, StyleSheet} from "react-native";
 import { TweetType } from "../types";
+import { Entypo, EvilIcons } from '@expo/vector-icons'; 
+import IconButton from "./IconButton";
 
 
 type TweetProps = {
@@ -11,10 +13,26 @@ const Tweet = ({tweet} : TweetProps) => {
 
     return (
         <View style={styles.container}>
-            <Image src={tweet.user.image} style={styles.userImage}/>
+            <Image source={{uri : tweet.user.image}} style={styles.userImage}/>
             <View style={styles.mainContainer}>
-                <Text style={styles.name}>{tweet.user.name}</Text>
+                <View style={{flexDirection: "row"}}>
+                    <Text style={styles.name}>{tweet.user.name}</Text>
+                    <Text style={styles.username}>{tweet.user.username} .2h</Text>
+                    <Entypo name="dots-three-horizontal" size={16} color="gray" style={{marginLeft: "auto"}} />
+                </View>
+                
                 <Text>{tweet.content}</Text>
+
+
+                {tweet.image && <Image src={tweet.image} style={styles.image}/>}
+
+                <View style={styles.footer}>
+                    <IconButton icon="comment" text={tweet.numberOfComments}/>
+                    <IconButton icon="retweet" text={tweet.numberOfRetweets}/>
+                    <IconButton icon="heart" text={tweet.numberOfLikes}/>
+                    <IconButton icon="chart" text={tweet.impressions || 0}/>
+                    <IconButton icon="share-apple"/>
+                </View>
             </View>
         </View>
     )
@@ -29,6 +47,7 @@ const styles = StyleSheet.create({
       borderColor: 'lightgrey',
       backgroundColor: "white",
     },
+    
     userImage: {
       width: 50, 
       height: 50, 
@@ -41,9 +60,24 @@ const styles = StyleSheet.create({
     name: {
       fontWeight: "600"
     },
+    username: {
+        color: "gray",
+        marginLeft: 5
+    },
     content: {
       lineHeight: 20,
       marginTop: 5
+    },
+    image: {
+        width: "100%",
+        aspectRatio: 16 / 9,
+        marginTop: 10,
+        borderRadius: 15
+    },
+    footer: {
+        flexDirection: 'row',
+        marginVertical: 5,
+        justifyContent: "space-between"
     }
   });
   
